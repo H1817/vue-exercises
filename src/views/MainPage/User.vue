f15e4cd2032705afa66294ffd5df48cf7feb9843
Huang Enrong <99640168+huangenrong@users.noreply.github.com>
2022-06-21 08:14:09 +0800

    Add files via upload

---
+++ src/views/MainPage/User.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <!-- 头像，微信名，账号 -->
+    <Self></Self>
+    <!-- 服务，收藏，设置等单元格 -->
+    <UserCell></UserCell>
+    <VanBar></VanBar>
+  </div>
+</template>
+
+<script>
+import Self from "@/components/ImportPart/User/Head.vue";
+import UserCell from "@/components/ImportPart/User/UserCell.vue";
+import VanBar from "@/components/ImportPart/Public/VanBar.vue";
+export default { components: { Self, UserCell, VanBar } };
+</script>
+
+<style scoped>
+</style>
---
+++ src/views/OtherPage/UsedInHome/AddFriend.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <InsideNav sign="添加好友"></InsideNav>
+    <van-search placeholder="账号/手机号" />
+    <p>我的微信号：enrong1234567</p>
+  </div>
+</template>
+
+<script>
+import InsideNav from "@/components/SmallPart/InsideNav.vue";
+export default {
+  components: { InsideNav },
+};
+</script>
+
+<style scoped>
+p {
+  text-align: center;
+  font-size: 10px;
+  color: silver;
+  margin: 20 0 0 0;
+}
+/* 搜索框离页面顶部距离 */
+.van-search {
+  margin: 46px 0 0 0;
+}
+</style>
